55ad6b761bac8ec13fc3af97466af18d274baa00
Aman Kubanychbek <amankubanychbek@gmail.com>
2019-08-15 06:00:36 +0000

    add dummy molecules/carousel and make it render

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log('Hey!')
+import React from 'react'
+import { render } from 'react-dom'
+
+import { Carousel } from './molecules/carousel'
+
+const rootEl = document.querySelector('#root')
+
+render(<Carousel />, rootEl)
---
+++ src/molecules/carousel.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Carousel = props => {
+    return <span>42</span>
+}
+
+Carousel.defaultProps = {}
+
+Carousel.propTypes = {}
+
+export { Carousel }
